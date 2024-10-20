0039460d36260925be5d12cca8af2a1b8d7603f0
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2024-10-20 11:29:15 -0500

    Escenario y decoracion del nivel

--- GP_2D/Assets/Scripts/CameraScript.cs
+++ GP_2D/Assets/Scripts/CameraScript.cs
@@ -7,16 +7,12 @@ public class CameraScript : MonoBehaviour
 
     public GameObject John;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 position = transform.position;
-        position.x = Camera.main.transform.position.x;
+        position.x = John.transform.position.x;
+        transform.position = position;
     }
 }
